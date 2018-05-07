93d4d4bc58f0ed1aa61377625df1174175666881
Windi <windi@hacktiv8.com>
2018-05-07 09:16:19 +0700

    Template

---
+++ mango_tree.js
@@ -0,0 +1,68 @@
+"use strict"
+
+// release 0
+
+class MangoTree {
+
+  // Initialize a new MangoTree
+  constructor() {
+  }
+
+  get age() {
+  }
+
+  get height() {
+
+  }
+
+  get fruits() {
+  }
+
+  get healtyStatus() {
+  }
+
+  get harvested() {
+
+  }
+
+
+  // Get current states here
+
+  // Grow the tree
+  grow() {
+  }
+
+  // Produce some mangoes
+  produceMangoes() {
+  }
+
+  // Get some fruits
+  harvest() {
+  }
+
+}
+
+class Mango {
+  // Produce a mango
+  constructor() {
+  }
+}
+
+/**
+  * driver code untuk release 0
+  * let mangoTree = new MangoTree()
+  * do {
+  *   mangoTree.grow();
+  *   mangoTree.produceMangoes();
+  *   mangoTree.harverst();
+  *   console.log(`[Year ${tree.age} Report] Height = ${tree.height} | Fruits harvested = ${tree.harvested}`)
+  * } while (mangoTree.healthyStatus != false)
+  */
+
+// Release 1
+class AppleTree {}
+class Apple {}
+
+// Release 2
+class FruitTree {}
+class Fruit {}
