4e69cbcc213d821accc1ae3d8e4656f18c77e0d8
Tushar <tusharbedi7373@hotmail.com>
2018-05-08 01:16:59 +0700

    Completed Release 1 mango Tree only

--- mango_tree.js
+++ mango_tree.js
@@ -6,23 +6,33 @@ class MangoTree {
 
   // Initialize a new MangoTree
   constructor() {
+    this._age = 0;
+    this._height = 0;
+    this._fruits = 0;
+    this._health = true;
+    this._harvested = 0;
+    this._matureAge = Math.round(Math.random() * (10 - 5) + 15)
+    this._maxAge = Math.round(Math.random() * (30 - 20) + 20)
   }
 
   get age() {
+    return this._age;
   }
 
   get height() {
-
+    return this._height;
   }
 
   get fruits() {
+      return this._fruits;
   }
 
-  get healtyStatus() {
+  get healthStatus() {
+    return this._health;
   }
 
   get harvested() {
-
+    return this._harvested;
   }
 
 
@@ -30,14 +40,32 @@ class MangoTree {
 
   // Grow the tree
   grow() {
+    //this._maxAge = Math.round(Math.random() * (30 - 20) + 20)
+    this._age += 1;
+    let height = this._height;
+    // console.log(this._age);
+    if (this._age < this._matureAge) {
+      this._height += Math.random() * 2;
+    }
+
+    if (this._age >= this._maxAge) {
+      this._health = false;
+    }
   }
 
   // Produce some mangoes
   produceMangoes() {
+
   }
 
   // Get some fruits
   harvest() {
+    // let growth = this.grow()
+    let randomHarvested = Math.floor(Math.random() * (5) + 20) + 1
+    if (this._age < this._maxAge) {
+      this._harvested += randomHarvested
+    }
+    // console.log(randomHarvested)
   }
 
 }
@@ -48,16 +76,19 @@ class Mango {
   }
 }
 
-/**
-  * driver code untuk release 0
-  * let mangoTree = new MangoTree()
-  * do {
-  *   mangoTree.grow();
-  *   mangoTree.produceMangoes();
-  *   mangoTree.harverst();
-  *   console.log(`[Year ${tree.age} Report] Height = ${tree.height} | Fruits harvested = ${tree.harvested}`)
-  * } while (mangoTree.healthyStatus != false)
-  */
+
+   // driver code untuk release 0
+   let mangoTree = new MangoTree()
+   do {
+     mangoTree.grow();
+     mangoTree.produceMangoes();
+     mangoTree.harvest();
+     console.log(`[Year ${mangoTree.age} Report] Height = ${mangoTree.height.toFixed(1)}m | Fruits harvested = ${mangoTree.harvested}`)
+     /* to fixed is for rounding to 1 decimal point */
+   } while (mangoTree._health != false)
+
+   console.log (`The tree has met its end. :(`)
+
 
 // Release 1
 class AppleTree {}
