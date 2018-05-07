8d485efc5173c5cac6021d7d2dcaac51b88ae3f8
handipriyono <handimacbook@gmail.com>
2018-05-07 23:48:17 +0700

    add release0 mango

--- mango_tree.js
+++ mango_tree.js
@@ -6,58 +6,100 @@ class MangoTree {
 
   // Initialize a new MangoTree
   constructor() {
+
+    this._height = 0
+    this._total = 0
+    this._fruit = 0
+    this._harvested = 0;
+    this._status = true
+
+    this._age = 0;
+    this._maxAge = 10;
+    this._mature = 3;
+
+    this._stopHeight = 3
+    this._bad = 0;
+    this._good = 0
+
+
   }
 
+
   get age() {
+    return this._age
   }
 
   get height() {
-
+    return this._height
   }
-
   get fruits() {
+    return this._fruit
   }
 
   get healtyStatus() {
+    return this._status
   }
 
   get harvested() {
 
-  }
+    return this._harvested
 
+  }
 
   // Get current states here
 
   // Grow the tree
   grow() {
+    let tinggi = (Math.random() * 1).toFixed(2)
+    //age
+    if (this._age < this._maxAge) {
+      this._age += 1
+    } else {
+      this._status = false
+    }
+
+    //height
+    if (this._age < this._stopHeight) {
+      this._height += tinggi
+    }
+
   }
 
   // Produce some mangoes
   produceMangoes() {
+    if (this._age > this._mature && this._age < this._maxAge) {
+      this._fruit += Math.floor(Math.random() * 10);
+    }
   }
 
   // Get some fruits
   harvest() {
+    this._harvested += this._fruit
+    let bad = parseInt(Math.random() * this._harvested)
+    this._bad = bad
+    this._good = this._harvested - bad
   }
 
 }
 
 class Mango {
   // Produce a mango
-  constructor() {
+  constructor(quality) {
+
   }
 }
 
+
 /**
-  * driver code untuk release 0
-  * let mangoTree = new MangoTree()
-  * do {
-  *   mangoTree.grow();
-  *   mangoTree.produceMangoes();
-  *   mangoTree.harverst();
-  *   console.log(`[Year ${tree.age} Report] Height = ${tree.height} | Fruits harvested = ${tree.harvested}`)
-  * } while (mangoTree.healthyStatus != false)
-  */
+ * driver code untuk release 0
+ * let mangoTree = new MangoTree()
+ * do {
+ *   mangoTree.grow();
+ *   mangoTree.produceMangoes();
+ *   mangoTree.harverst();
+ *   console.log(`[Year ${tree.age} Report] Height = ${tree.height} | Fruits harvested = ${tree.harvested}`)
+ * } while (mangoTree.healthyStatus != false)
+ */
 
 // Release 1
 class AppleTree {}
